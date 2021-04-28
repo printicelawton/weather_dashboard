0f02dcefbb396d93db893c1d1c753d3d66f173dd
Printice Lawton <printicelaw10@gmail.com>
2021-04-28 09:04:48 -0400

    Trying to get to deploy via Heroku

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ async function weather(location){
 }
 
 async function weatherApp(location){
-    const baseURL = `http://api.weatherstack.com/current?access_key=${key}&query=${location}`;
+    const baseURL = `https://cors-anywhere.herokuapp.com/http://api.weatherstack.com/current?access_key=${key}&query=${location}`;
     const res = await fetch(baseURL);
     const data = await res.json();
     console.log(data)
